46993dd3a9d14fb4c0d36c6b5e16fcadd5ea576b
hhlass <1123835773@qq.com>
2019-05-23 10:56:15 +0800

    新增淘宝数据获取

--- PythonUtils/get_prices/GetPrices.py
+++ PythonUtils/get_prices/GetPrices.py
@@ -3,7 +3,7 @@
 ##  安装有 requests_html 模块
 ##  运行环境安装有 sendmail
 
-## 单行线程运行
+##  单行线程运行
 
 from requests_html import HTMLSession
 import smtplib
@@ -16,6 +16,11 @@ import hashlib
 
 session = HTMLSession()     #连接对象
 hashm = hashlib.md5()
+float_max=float("inf")
+my_mail=""
+sender=""
+code = ""
+sendname=""
 
 # 设置日志对象
 logging.basicConfig(level=logging.INFO,
@@ -46,8 +51,12 @@ tb_user_agent=""
 #设置京东价格类型 p:折扣后的价格, tpp:plus会员价, m:原价,op:暂时不知,一般和p相同
 jtype="tpp"
 
+
 # 实时汇率api
-cover_url="https://free.currconv.com/api/v7/convert?q={}&compact=ultra&apiKey=eff1240539fa23bc7474"
+cover_url=""
+
+
+##################################
 
 
 # 获取已发送列表
@@ -73,7 +82,7 @@ def __amazonGet(url):
         return (product,__coverMoney("USD",float(nprice)))
     except:
         logger.error("获取美亚数据报错 --> {}".format(url))
-        return ("",0.00)
+        return ("",float_max)
 
 # 提取京东商品的sku
 def __getSKU(url):
@@ -95,25 +104,52 @@ def __jdGet(url):
         return (product,nprice)
     except:
         logger.error("获取京东数据报错 --> {}".format(url))
-        return ("",0.00)
+        return ("",float_max)
 
-#获取淘宝数据
-def __taobaoGet(url):
+#获取天猫数据
+def __tmailGet(url):
     try:
+        logger.info("获取天猫数据")
         r = __getHtml(url)
         product = r.html.xpath("//h1")[1].text
         jurl = "https:" + re.findall(r"//mdskip\.taobao\.com/core/initItemDetail\.htm.+?\"",r.text)[0][:-1]
         headers={"cookie":tb_cookie,"referer":url,"user-agent":tb_user_agent}
         r2 = __getHtml1(jurl, headers)
         prices = re.findall(r"\"price\":\"(.+?)\"",r2.text)
-        nprice=float(prices[0])
+        nprice=float_max
         for p in prices:
             if float(p)<nprice:
                 nprice=float(p)
         return (product,nprice)
     except:
+        logger.error("天猫数据获取失败! --> {}".format(url))
+        return ("",float_max)
+
+def __taobaoGet(url,flag):
+    try:
+        logger.info("获取淘宝数据")
+        r = __getHtml(url)
+        product = r.html.xpath("//h3")[0].text+" --> {} ".format(flag)
+        pString = re.findall(r"propertyMemoMap: (.+?})",r.text)[0]
+        pJson = json.loads(pString)
+        newP={}
+        for k,v in pJson.items():
+            newP[v]=k
+        jurl = "https:" + re.findall(r"//detailskip\.taobao\.com/service/getData/1/p1/item/detail/sib\.htm.+?\'",r.text)[0][:-1]+"&callback=onSibRequestSuccess"
+        headers={"cookie":tb_cookie,"referer":url,"user-agent":tb_user_agent}
+        r2 = __getHtml1(jurl, headers)
+        startFlag = ";{};".format(newP[flag])
+        prices=re.findall(r"\"%s\":(.+?})" %startFlag,r2.text)
+        nprice = float_max
+        for n in prices:
+            nn=json.loads(n.replace("[","").replace("]","")).get("price")
+            if nn!= None and float(nn)<nprice:
+                nprice=float(nn)
+        return (product,nprice)
+    except Exception as e:
+        logger.error(e)
         logger.error("淘宝数据获取失败! --> {}".format(url))
-        return ("",0.00)
+        return ("",float_max)
 
 # 美元USD , 人民币CNY
 def __coverMoney(fm,price):
@@ -155,14 +191,12 @@ def __sendEMail(text,mailTo):
         logger.info("{} ^|^ 今天已经发送过!".format(hashm.hexdigest()))
         return
     logger.info("开始发送邮件!")
-    sender="<mail_address>"
-    receivers=["<mail_address>"]
+    receivers=[my_mail]
     for i in mailTo.split(","):
         receivers.append(i)
     names =receivers
-    code="<password>"
     message = MIMEText(text, "plain", "utf-8")
-    message["From"]="hhlass <{}>".format(sender)
+    message["From"]="{} <{}>".format(sendname,sender)
     message["To"]=__createToMessage(receivers,names)
     title = "Prices数据监控"
     message["Subject"]=Header(title,"utf-8")
@@ -170,17 +204,19 @@ def __sendEMail(text,mailTo):
         smt = smtplib.SMTP_SSL("smtp.qq.com",465)
         smt.login(sender,code)
         smt.sendmail(sender,receivers,message.as_string())
-        logger.info("邮件发送成功!")
+        logger.info("邮件发送成功! 接受人:{}".format(str(receivers)))
         __writeHasSended(md5)
         smt.quit()
     except Exception as e:
         logger.error(e)
         logger.error("邮件发送失败!")
 
+################################################
+
 # 获取相应商品的当前价格
 def getNowPrice():
     product=""
-    nprice=0.00
+    nprice=float("inf")
     for item in jk_iterms["all"]:
         url = item["url"]
         pt = item["pt"]
@@ -189,10 +225,15 @@ def getNowPrice():
         elif url.startswith("https://item.jd.com/"):
             product,nprice=__jdGet(url)
         elif url.startswith("https://detail.tmall.com"):
-            product,nprice=__taobaoGet(url)
-        if nprice<pt and nprice>0.00:
+            product,nprice=__tmailGet(url)
+        elif url.startswith("https://item.taobao.com"):
+            flag = item["flag"]
+            product, nprice = __taobaoGet(url,flag)
+        if nprice<pt:
             __sendEMail("物品<<{}>> ({}) 的当前价格 {}CNY 小于设定价格 {}CNY 。".format(product, url, str(nprice), str(pt)),item["mailTo"])
         logger.info("物品: {}, 网址: {}, 当前价格: {}, 目标价格: {} \n".format(product,url,str(nprice),str(pt)))
+
+#################################
 
 if __name__ == '__main__':
     sendedlist = __getSended()
